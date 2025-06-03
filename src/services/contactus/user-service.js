0089948026f652789cc50397a1a4f56398b0174f
Abhis0801 <abhishek@brilliantechsoft.com>
2025-06-03 11:23:04 +0530

    added field status in user model set default status pending

--- src/services/contactus/user-service.js
+++ src/services/contactus/user-service.js
@@ -7,12 +7,12 @@ const sendEmail = require('../../utils/send-Email');
 const userService = new UserRepository();
 const createUser = async (userData) => {
   try {
-    // Create a new user in the database
     const user = await userService.create(userData);
-    
-    // Send notification email on new contact form submission
+
+    user.status = 'PENDING';
+    await user.save(); // Save the updated status
+
     await sendEmail(user);
-    
     return user;
   } catch (error) {
     throw new Error(`Error creating user: ${error.message}`);
