f70649c5d297c2e4571b4bf92d98dbb46419ddad
Devashish <devashish@brilliantechsoft.com>
2025-06-04 18:10:46 +0530

    unique-email

--- src/controllers/contactus/user-controller.js
+++ src/controllers/contactus/user-controller.js
@@ -1,15 +1,24 @@
-
 const logger = require('../../config/logger');
 const { userService } = require('../../services/contactus');
 const sendEmail = require('../../utils/send-Email');
+const { User } = require('../../models'); // Import the User model
 
 exports.createUser = async (req, res, next) => {
   try {
+    const { email } = req.body;
+
+    // Check for existing user by email
+    const existingUser = await User.findOne({ where: { email } });
+    if (existingUser) {
+      return res.status(400).json({ message: 'Email already exists. Please use a different email.' });
+    }
+
+    // Proceed to create new user
     console.log("Creating user with data:", req.body);
     const user = await userService.createUser(req.body);
     logger.info(`User created: ${user.id} - ${user.email}`);
 
-    // Send notification email on new contact form submission
+    // Send notification email
     await sendEmail(user);
     logger.info(`Notification email sent for user: ${user.id}`);
 
@@ -19,4 +28,3 @@ exports.createUser = async (req, res, next) => {
     next(error);
   }
 };
-
