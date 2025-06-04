f4574da46f2e109f93c7b6e2649cb26beb1f8fe8
Abhis0801 <abhishek@brilliantechsoft.com>
2025-06-04 17:06:22 +0530

    extract id from token user

--- src/routes/v1/campaign-routes.js
+++ src/routes/v1/campaign-routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { CampaignController } = require('../../controllers');
 const { upload } = require('../../middlewares/upload-middleware');
 const validateCampaign = require('../../middlewares/validate-campaign');
+const { VerifyToken } = require('../../middlewares');
 
 
 const router = express.Router();
@@ -9,8 +10,8 @@ const router = express.Router();
 
 
 /* campaign routes */
-router.post('/createCampaign', upload.single('creativeFile'), validateCampaign, CampaignController.createCampaign);
-router.get('/getCampaigns', CampaignController.getCampaigns);
+router.post('/createCampaign',VerifyToken, upload.single('creativeFile'), validateCampaign, CampaignController.createCampaign);
+router.get('/getCampaigns', VerifyToken,CampaignController.getCampaigns);
 router.put('/:id/status', CampaignController.updateCampaignStatus);
 
 module.exports = router;
