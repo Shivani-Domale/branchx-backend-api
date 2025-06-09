df984f610aae8511956e0cc103d8726ae5671406
Abhis0801 <abhishek@brilliantechsoft.com>
2025-06-09 13:23:51 +0530

    clean code

--- src/routes/v1/auth-routes.js
+++ src/routes/v1/auth-routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { loginUser } = require('../../controllers/login/login-user');
+const { LoginUserController } = require('../../controllers');
 
-router.post('/login', loginUser);
+router.post('/login', LoginUserController.loginUser);
 module.exports = router;
