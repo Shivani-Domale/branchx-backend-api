cbc31a56dd7700a49380fd79a26c4f230db7396a
Abhis0801 <abhishek@brilliantechsoft.com>
2025-06-11 11:05:28 +0530

    add campaign based on device type and location

--- src/repositories/product-repository.js
+++ src/repositories/product-repository.js
@@ -1,10 +1,27 @@
 const crudRepository = require("./crud-repository");
-const {Product}  = require('../models');
+const { Product } = require('../models');
 
-class ProductRepository extends crudRepository{
-    constructor()
-    {
+class ProductRepository extends crudRepository {
+    constructor() {
         super(Product);
+    }
+
+    async findIdByProductType(productType) {
+        try {
+            const product = await this.model.findOne({
+                where: {
+                    product_type: productType.trim()
+                }
+            });
+
+            if (!product) {
+                throw new Error("Product type not found");
+            }
+
+            return product.id;
+        } catch (error) {
+            throw new Error(`Error fetching product by type: ${error.message}`);
+        }
     }
 }
 
