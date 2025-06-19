f4296cb19d46f35d2a878c945bddbcfde9e67bce
Devashish <devashish@brilliantechsoft.com>
2025-06-19 17:11:33 +0530

    forget password api for user's

--- src/utils/send-Email.js
+++ src/utils/send-Email.js
@@ -37,3 +37,21 @@ ${user.message}`,
     throw error;
   }
 };
+
+
+module.exports.sendEmail = async ({ to, subject, html }) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail', // or SMTP settings
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  await transporter.sendMail({
+    from: `"BranchX Support" <${process.env.EMAIL_USER}>`,
+    to,
+    subject,
+    html,
+  });
+};
